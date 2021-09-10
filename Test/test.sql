30ad075f2045ab0b973a9a872a69d0287e4619d8
Shivneel <Shivneel077@gmail.com>
2021-09-09 17:04:40 -0700

    Testing

---
+++ Test/test.sql
@@ -0,0 +1,12 @@
+DROP TABLE Test;
+
+CREATE TABLE Test (
+    col_1 CHAR(50) PRIMARY KEY,
+    num1_2 DECIMAL(1,2),
+    string INTEGER(20),
+    num2_3 DECIMAL(1,3)
+);
+
+INSERT INTO Test VALUES ('test',123123,'123',12345.12345);
+
+SELECT * FROM Test;
