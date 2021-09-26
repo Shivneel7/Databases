9772e56c358623f00d76d558f7846584226ac076
Shivneel Chand <Shivneel077@gmail.com>
2021-09-26 15:15:54 -0700

    Finished lab-3

--- lab-3/test/13.sql
+++ lab-3/test/13.sql
@@ -1,5 +1,5 @@
-select SUM(c_acctbal) from customer, nation, region
+select sum(c_acctbal) from customer, nation, region
 WHERE r_name = 'EUROPE'
-and r_regionkey = n_nationkey
+and r_regionkey = n_regionkey
 and n_nationkey = c_nationkey
 and c_mktsegment = 'MACHINERY';
