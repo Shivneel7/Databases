5877090023d723c03b468ca520f6608062db00d5
Shivneel Chand <Shivneel077@gmail.com>
2021-09-26 07:40:53 -0700

    Finished first 6 queries

---
+++ lab-3/test/6.sql
@@ -0,0 +1,6 @@
+select distinct n_name from nation, customer, orders
+where customer.c_nationkey = nation.n_nationkey
+and o_custkey = c_custkey
+and o_orderdate >= '1996-09-10'
+and o_orderdate <= '1996-09-12'
+order by n_name ASC;
