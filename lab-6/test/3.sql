cba69f7b67454d2e49596388edf846e5a8ae4332
Shivneel Chand <Shivneel077@gmail.com>
2021-10-16 02:16:40 -0700

    did 3 and 4

--- lab-6/test/3.sql
+++ lab-6/test/3.sql
@@ -11,13 +11,26 @@
 --     )
 -- ;
 
-select count(s_suppkey)
+select count(distinct p_partkey)
 from (select p_partkey
-    from part, supplier s1, nation n1, partsupp, supplier s2, nation n2
+    from part, supplier, nation, partsupp
     where p_partkey = ps_partkey
     and ps_suppkey = s_suppkey
     and s_nationkey = n_nationkey
-    and s_nationkey = n_nationkey
     and n_name = 'UNITED STATES'
-    )
+    GROUP BY
+	p_partkey
+    HAVING
+	count(ps_suppkey) =2);
 ;
+
+-- select p_partkey
+--     from part, supplier, nation, partsupp
+--     where p_partkey = ps_partkey
+--     and ps_suppkey = s_suppkey
+--     and s_nationkey = n_nationkey
+--     and n_name = 'UNITED STATES'
+--     GROUP BY
+-- 	p_partkey
+--     HAVING
+-- 	count(ps_suppkey) =2;
--- lab-6/test/4.sql
+++ lab-6/test/4.sql
@@ -1,9 +1,12 @@
-select count(p_partkey)
-from (select p_partkey
+select count(distinct s_suppkey)
+from (select s_suppkey
     from part, supplier, nation, partsupp
     where p_partkey = ps_partkey
     and ps_suppkey = s_suppkey
     and s_nationkey = n_nationkey
-    and s_name = 'UNITED STATES'
-    )
+    and n_name = 'UNITED STATES'
+    GROUP BY
+	ps_suppkey
+    HAVING
+	count(p_partkey) > 40);
 ;
